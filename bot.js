38b5a9ee1c79bdf950cd8178a88bde55d66fa510
Reem35 <43762012+Reem35@users.noreply.github.com>
2018-10-29 18:05:46 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,13 @@
-const Discord = require("discord.js");
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : EX Clan`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`' WéSKKyẋ - | Brodcast ,`,"http://twitch.tv/akrame35")
+client.user.setStatus("dnd")
+});const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = ".";
 client.on('message', message => { // Leaked by [ @M3a4x ]
@@ -48,14 +57,5 @@ msg.delete();
 })
 }
 });
-client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Desert Bot- Script By : EX Clan`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`' WéSKKyẋ - | Brodcast ,`,"http://twitch.tv/Death Shop")
-client.user.setStatus("dnd")
-});
+
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
